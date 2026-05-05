d4668a622502d5aa57723193bc81cca2752d30a1
David Viodes <david.viodes@compassmining.io>
2026-05-04 17:16:01 -0700

    feat(math_and_physics) added train_stop_distance rust calculation code

--- math_and_physics/src/lib.rs
+++ math_and_physics/src/lib.rs
@@ -2,12 +2,15 @@ use pyo3::prelude::*;
 
 mod drop_time;
 mod straight_line_acceleration;
+mod train_stop_distance;
 
 #[pymodule]
 fn math_and_physics(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<drop_time::DropTimeResult>()?;
     m.add_class::<straight_line_acceleration::StraightLineAccelerationResult>()?;
+    m.add_class::<train_stop_distance::TrainStopDistanceResult>()?;
     m.add_function(wrap_pyfunction!(drop_time::compute_drop_time, m)?)?;
     m.add_function(wrap_pyfunction!(straight_line_acceleration::compute_straight_line_acceleration, m)?)?;
+    m.add_function(wrap_pyfunction!(train_stop_distance::compute_train_stop_distance, m)?)?;
     Ok(())
 }
---
+++ math_and_physics/src/train_stop_distance.rs
@@ -0,0 +1,101 @@
+use pyo3::prelude::*;
+
+#[pyclass]
+pub struct TrainStopDistanceResult {
+    #[pyo3(get)]
+    pub hit: bool,
+    #[pyo3(get)]
+    pub correct_stopping_distance: f64,
+    #[pyo3(get)]
+    pub train_positions: Vec<f64>,
+}
+
+fn compute_train_stop_distance_logic(
+    input_distance: f64,
+    train_velocity: f64,
+    train_deceleration: f64,
+) -> TrainStopDistanceResult {
+    let dt: f64 = 1.0 / 60.0;
+    let mut train_positions: Vec<f64> = vec![];
+
+    let correct_train_stop_distance: f64 = train_velocity.powi(2) / (2.0 * train_deceleration);
+    let rounded_correct_train_stop_distance: f64 = (correct_train_stop_distance * 10.0).round() / 10.0;
+    let student_train_stop_distance: f64 = (-input_distance * 10.0).round() / 10.0;
+
+    let time: f64 = -train_velocity / train_deceleration;
+
+    let mut t: f64 = 0.0;
+    while t <= time {
+        let train_position: f64 =
+            input_distance
+            + (train_velocity * t)
+            + (0.5 * train_deceleration * t.powi(2));
+
+        train_positions.push(train_position);
+
+        t += dt;
+    }
+
+    if student_train_stop_distance == rounded_correct_train_stop_distance {
+        TrainStopDistanceResult{
+            hit: true,
+            correct_stopping_distance: rounded_correct_train_stop_distance,
+            train_positions: train_positions
+        }
+    } else {
+        TrainStopDistanceResult{
+            hit: false,
+            correct_stopping_distance: rounded_correct_train_stop_distance,
+            train_positions: train_positions
+        }
+    }
+}
+
+#[pyfunction]
+pub fn compute_train_stop_distance(
+    input_distance: f64,
+    train_velocity: f64,
+    train_deceleration: f64,
+) -> PyResult<TrainStopDistanceResult> {
+    Ok(compute_train_stop_distance_logic(
+        input_distance,
+        train_velocity,
+        train_deceleration,
+    ))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_compute_train_stop_distance_logic() {
+        const TRAIN_VELOCITY: f64 = 19.45;
+        const TRAIN_DECELERATION: f64 = -0.18;
+
+        let distance_calculation: TrainStopDistanceResult = compute_train_stop_distance_logic(
+            1050.8,
+            TRAIN_VELOCITY,
+            TRAIN_DECELERATION
+        );
+
+        assert!(distance_calculation.hit);
+        assert_eq!(distance_calculation.correct_stopping_distance, 1050.8);
+        assert!(distance_calculation.train_positions.len() > 0);
+    }
+
+    #[test]
+    fn test_failed_compute_train_stop_distance_logic() {
+        const TRAIN_VELOCITY: f64 = 19.45;
+        const TRAIN_DECELERATION: f64 = -0.18;
+
+        let distance_calculation: TrainStopDistanceResult = compute_train_stop_distance_logic(
+            950.8,
+            TRAIN_VELOCITY,
+            TRAIN_DECELERATION
+        );
+
+        assert!(!distance_calculation.hit);
+        assert_eq!(distance_calculation.correct_stopping_distance, 1050.8);
+    }
+}
