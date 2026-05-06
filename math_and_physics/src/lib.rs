b8604082c522c6136862f41765e966d8c8001d94
David Viodes <david.viodes@compassmining.io>
2026-05-05 19:25:47 -0700

    feat(math_and_physics): added bird migration instantaneous velocity question type

--- math_and_physics/src/lib.rs
+++ math_and_physics/src/lib.rs
@@ -3,14 +3,17 @@ use pyo3::prelude::*;
 mod drop_time;
 mod straight_line_acceleration;
 mod train_stop_distance;
+mod bird_instantaneous_velocity;
 
 #[pymodule]
 fn math_and_physics(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<drop_time::DropTimeResult>()?;
     m.add_class::<straight_line_acceleration::StraightLineAccelerationResult>()?;
     m.add_class::<train_stop_distance::TrainStopDistanceResult>()?;
+    m.add_class::<bird_instantaneous_velocity::BirdInstantaneousVelocityResult>()?;
     m.add_function(wrap_pyfunction!(drop_time::compute_drop_time, m)?)?;
     m.add_function(wrap_pyfunction!(straight_line_acceleration::compute_straight_line_acceleration, m)?)?;
     m.add_function(wrap_pyfunction!(train_stop_distance::compute_train_stop_distance, m)?)?;
+    m.add_function(wrap_pyfunction!(bird_instantaneous_velocity::compute_bird_instantaneous_velocity, m)?)?;
     Ok(())
 }
